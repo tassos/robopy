eb05b3ac8b6bf8831db5143646f25fba6e103572
Gary Deschaines <gedeschaines@gmail.com>
2019-02-02 23:49:34 -0600

    Uses figure numbering (fign) system.

--- notebooks/dListMPL/dListMPL.py
+++ notebooks/dListMPL/dListMPL.py
@@ -22,6 +22,7 @@ rc('animation', html='jshtml')    # Matplotlib animations will be HTML wrapped J
 # Import requisite robopy modules.
 import _robopy                                     # locates desired robopy module
 from robopy.base.graphics import GraphicsRenderer  # to select graphics renderer
+import robopy.base.graphics_mpl as gmpl            # for rgb_named_colors
 import robopy.base.display_list as dList           # to use display lists
 import robopy.base.transforms as tr                # to apply transforms
 from robopy.base.mesh_geoms import *               # to use mesh geometric shapes
@@ -31,8 +32,8 @@ import numpy as np                                 # to use NumPy ndarray type
 # In[2]:
 
 
-# Define some GraphicsIPV parameters which will be used in plot()
-# method calls in following cells.
+# Define some GraphicsMPL parameters which will be used in the
+# Mpl3dArtist.plot() method calls in following cells.
 
 dMode = 'IPY'
 limits = [-0.5, 0.5, -0.5, 0.5, -0.5, 0.5]
@@ -41,7 +42,8 @@ limits = [-0.5, 0.5, -0.5, 0.5, -0.5, 0.5]
 # In[3]:
 
 
-# Define an object.
+# Define a mesh grid geometric object.
+
 cyl = cylinder(0.2,0.2,0.05,0.1)  # create a cylinder
 
 
@@ -49,6 +51,7 @@ cyl = cylinder(0.2,0.2,0.05,0.1)  # create a cylinder
 
 
 # Create two transformation matrices.
+
 Rx = np.array( [[1,0,0,0], [0,0,-1,0], [0,1,0,0],[0,0,0,1]])
 Ry = np.array( [[0,0,1,0],[0,1,0,0],[-1,0,0,0],[0,0,0,1]] )
 
@@ -57,7 +60,8 @@ Ry = np.array( [[0,0,1,0],[0,1,0,0],[-1,0,0,0],[0,0,0,1]] )
 
 
 # Create a DisplayList with 3 instances of the cylinder,
-# keep the DisplayListItem references
+# keep the DisplayListItem references.
+
 dl = dList.DisplayList()
 dl1 = dl.add('surface', 'cyl1', cyl, color='blue')
 dl2 = dl.add('surface', 'cyl2', cyl, color='red')
@@ -67,7 +71,8 @@ dl3 = dl.add('surface', 'cyl3', cyl, color='green')
 # In[6]:
 
 
-# Transform two of the cylinders by setting the transform field
+# Transform two of the cylinders by setting the transform field.
+
 dl2.transform = Rx
 dl3.transform = Ry
 
@@ -75,23 +80,40 @@ dl3.transform = Ry
 # In[7]:
 
 
-# Obtain a robopy graphics renderer which utilizes ipyvolume.
-# Currently an empty Figure XYZ axes system will be displayed.
-gMpl = GraphicsRenderer('MPL')  # sets graphics.gRenderer
+# Obtain a robopy graphics renderer which utilizes matplotlib.
 
+gMpl = GraphicsRenderer('MPL')  # sets graphics.gRenderer; returns Mpl3dArtist object
 
-# In[8]:
-
-
-# Give graphics renderer the DisplayList to plot (gRenderer.plot()).
-gMpl = GraphicsRenderer('MPL')  # sets graphics.gRenderer (creates new figure)
-gMpl.plot(dl, limits=limits)
+# Figure 0 with an empty XYZ axes system will be displayed below.
 
 
 # In[9]:
 
 
+# Display a mesh grid geometric object to show the default figure properties.
+
+rgb = gmpl.rgb_named_colors(['purple'])
+gMpl.plot_parametric_shape('sphere', solid=True, c=rgb[0])
+gMpl.show()
+
+# The purple sphere should be displayed in Figure 0 above.
+
+
+# In[10]:
+
+
+# Give a graphics renderer the DisplayList to plot (gRenderer.plot()).
+
+gMpl.close()                    # close current figure
+gMpl = GraphicsRenderer('MPL')  # sets graphics.gRenderer (creates new Figure 0)
+gMpl.plot(dl, limits=limits)    # plot display list 'dl' created above.
+
+
+# In[11]:
+
+
 # Define transform function to animate DisplayListItems.
+
 def transFunc(t):
     """
     Sample transformation function to rotate display list
@@ -102,19 +124,22 @@ def transFunc(t):
     return tr.trotx(2.0*t, unit="deg")
 
 
-# In[10]:
+# In[12]:
 
 
-# Give graphics renderer the DisplayList to animate.
-gMpl = GraphicsRenderer('MPL')  # sets graphics.gRenderer (creates new figure)
+# Give the graphics renderer the DisplayList to animate.
+
+gMpl.close()                    # closes current figure
+gMpl = GraphicsRenderer('MPL')  # sets graphics.gRenderer (creates new Figure 0)
 gMpl.animate(dl, transFunc, duration=5.0, frame_rate=30, limits=limits)
 
 
-# In[11]:
+# In[13]:
 
 
 # Wait until the animation completes before running this cell to create a controlled animation.
-gMpl.anim
+
+gMpl.getAnimDL()
 
 
 # In[ ]:
