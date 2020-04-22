56f8a4a38af4236f19fe87fec27ba7aaedec2158
Gary Deschaines <gedeschaines@gmail.com>
2020-04-21 21:54:48 -0500

    Fixed spelling error.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     version='1.0.8.0',
 
     description='A Python 3 implementation of Peter Corke\'s Robotics Toolbox',
-    long_description='ROBOPY is a python implimentation of The Robotics Toolbox (RTB) (The brainchild of Peter Corke) '
+    long_description='ROBOPY is a Python implementation of The Robotics Toolbox (RTB) (The brainchild of Peter Corke) '
                      'for Matlab. The toolbox provides a range of functions that are useful in the study and '
                      'simulation of classical arm-type robotics including kinematics, dynamics and trajectory '
                      'generation. Via the porting of the toolbox we hope to create greater access to the toolbox '
